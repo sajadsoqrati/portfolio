8c209868c6bd8cc0392d22c88c750d7f25958ea0
sajadsoqrati <sajadsoqrati@gmail.com>
2025-08-25 18:06:16 +0330

    converting all images format to WebP for better performance

--- src/assets/components/Projects.tsx
+++ src/assets/components/Projects.tsx
@@ -10,32 +10,32 @@ const Projects = () => {
   const projects: project[] = [
     {
       id: "jahatstudio",
-      imgSrc: "/imges/jahatstudio.png",
+      imgSrc: "/imges/jahatstudio.webp",
       caption:
         "jahatstudio یک پروژه فریلنس مربوط به استدیو تبلیغاتی جهت هستش که به سفارش این مجموعه با دیزاین مد نظرشون کد نویسی کردم  در بخش فرانت از تکنولوژی های React- typeScript- TailwindCss4-React Router DOM -React Hook Form - Zod - ReactSlick - vite استفاده کردم و برای بخش بک اند هم از Nodejs-Express .استفاده کردم ولی هنوز خیلی جای کارداره که انجامش به نظر کارفرما بستگی داره",
     },
     {
       id: "assemblyendgame",
-      imgSrc: "/imges/Assmebly.png",
+      imgSrc: "/imges/assmebly.webp",
       caption:
         " اسمبلی اند گیم یک بازی حدس کلمه است که با هر  بار حدس اشتباه یک زبان برنامه نویسی از بین میرود و در اخر فقط زبان اسمبلی در دنیا باقی میماند این پروژه با reactنوشته شده و از clsx برای مدیریت conditional CSS classes و ازreact-confetti برای نمایش انیمیشن جشن در صورت پیروزی استفاده شده. و انتخاب کلمات به صورت رندوم از دیتابیس 485 کلمه ای انجام میشود",
     },
     {
       id: "amazonpracticee",
-      imgSrc: "public/imges/amazon-checkout.png",
+      imgSrc: "public/imges/amazon-checkout.webp",
       caption:
         " این پروژه تمرینی یک وب‌سایت فروشگاهی به سبک آمازون هست که به صورت کامل با JavaScript، HTML و CSS پیاده‌سازی شده وشما میتونید محصولات مورد نظر رو به لیست خریدتون اضافه کنید و از بالا سمت راست میتونید سبد خرید رو باز کنید و قیمت های محاسبه شده رو مشاهده کنید و هر تغیری که خواستید رو روی محصولات اعمال کنید حتی زمان ارسال محصول هم میتونید تغییر بدید و قیمت نهایی به نسبت این تغییرات عوض میشه در کل سعی کردم یه پروژه کاملا اینتراکتیو رو با جاوا اسکریپت بزنم",
     },
 
     {
       id: "sajad-tenzies",
-      imgSrc: "/imges/tenzies.png",
+      imgSrc: "/imges/tenzies.webp",
       caption:
         "Tenziesیک پروژه سرگرم کننده هست که باreactنوشته شده وشما میتونید بازدن دکمه rollتاس هارو به گردش در بیارید و با کلیک کردن روی هر تاس میتونید ثابت نگهش دارید و با دوباره زدن rollبقیه تاس ها عددشون تغییر میکنه تا زمانی که همه تاس ها به یک عدد برسن وشما برنده بشید. ",
     },
     {
       id: "sajad-memegenerator",
-      imgSrc: "/imges/memegenerator.png",
+      imgSrc: "/imges/memegenerator.webp",
       caption:
         "میم جنریتور یک برنامه سرگرم کننده هستش که با reactنوشتم و کاربر میتونه متن بالا و پایین تصویر رو وارد کنه و بازدن دکمه get a new meme image برنامه یک میم تمپلیت جدید از imgflipAPI میاره(fetch)میکنه",
     },
